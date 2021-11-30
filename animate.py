6ca03caca7e50f6d7253b668e8db25b6305c3f1b
Minh H. Pham Dinh <phdminh01@gmail.com>
2021-11-30 21:08:13 +0700

    added 3d box

--- animate.py
+++ animate.py
@@ -9,8 +9,8 @@ scale = ".,-~:;=!*#$@"
 R1, R2 = (6,6)
 axis_x, axis_y, axis_z = (np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1]))
 init_r = np.array([cols/2,0,cols/2])
-dic, i = draw_donut(R1, R2, init_r)
-# dic, i = draw_box(20,20,20,init_r)
+# dic, i = draw_donut(R1, R2, init_r)
+dic, i = draw_box(15,15,30,init_r)
 light = np.array([0,-1,1])
 
 def vector_rotate(vec, angle, axis):
@@ -22,8 +22,8 @@ def vector_rotate(vec, angle, axis):
 for idx in range(i):
     r0 = dic[idx]["coord"] - init_r
     n0 = dic[idx]["n"]
-    r = vector_rotate(r0, -pi/6, axis_x) + init_r
-    n = vector_rotate(n0, -pi/6, axis_x)
+    r = vector_rotate(r0, -pi/3, axis_x) + init_r
+    n = vector_rotate(n0, -pi/3, axis_x)
     dic[idx] = {"coord": r, "n": n}
 
 def draw(terminal):
@@ -49,14 +49,14 @@ def draw(terminal):
         terminal.border(0)
         shade = [x for _, x in sorted(zip(points, shade), key=lambda pair: pair[0][1])]
         points.sort(key=lambda x: x[1])
-        min_illu = min(shade)
-        max_illu = max(shade)
+        min_illu, max_illu  = min(shade), max(shade)
         for idx in range(i):
             x, y, z = points[idx]
             illu = (shade[idx] - min_illu)*11/(max_illu - min_illu)
             string = scale[round(illu)]
             terminal.addstr(round(z * rows/cols), round(x), string, curses.A_BOLD)
         theta += 0.05
+        terminal.move(0,0)
         terminal.refresh()
 
 
--- shape/generator.py
+++ shape/generator.py
@@ -2,6 +2,7 @@ from math import pi
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 
+
 axis_x, axis_y, axis_z = (np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1]))
 
 def vector_rotate(vec, angle, axis):
@@ -36,9 +37,9 @@ def draw_donut(R1, R2, init_r):
 
 
 def draw_box(w, l, h, init_r):
-    width = np.arange(-w/2, w/2 + 1, 1)
-    length = np.arange(-l/2, l/2 + 1, 1)
-    height = np.arange(-h/2, h/2 + 1, 1)
+    width = np.linspace(-w/2, w/2, 15)
+    length = np.linspace(-l/2, l/2, 15)
+    height = np.linspace(-h/2, h/2, 15)
     dic = {}
     i = 0
     for x in width:
@@ -48,29 +49,28 @@ def draw_box(w, l, h, init_r):
             dic[i] = {"coord": r, "n": n}
             i += 1
 
-    count = 0
-
     for j in range(0, i, 1):
         x0, y0, z0 = (dic[j]["coord"] - init_r).tolist()
         if x0 in {-w/2, w/2} or y0 in {-l/2, l/2}:
             for z in height:
                 r = init_r + np.array([x0, y0, z])
-                if  -l/2 < y0 < l/2:
+                if  -l/2 <= y0 < l/2:
                     if x0 == -w/2:
                         n = np.array([-1,0,0])
                     elif x0 == w/2:
                         n = np.array([1,0,0])
-                elif  -w/2 < x0 < w/2:
+                elif  -w/2 <= x0 < w/2:
                     if y0 == -l/2:
                         n = np.array([0,-1,0])
                     elif y0 == l/2:
                         n = np.array([0,1,0])
                 dic[i] = {"coord": r, "n": n}
                 i += 1
-        # else:
-        r = init_r + np.array([x0, y0, -z0])
-        n = np.array([0,0,1])
-        dic[i] = {"coord": r, "n": n}
-        i += 1
 
+        else:
+            r = init_r + np.array([x0, y0, h/2])
+            n = np.array([0,0,1])
+            dic[i] = {"coord": r, "n": n}
+            i += 1
+    # print(count)
     return dic, i
