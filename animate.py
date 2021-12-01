be86176cb7b827638b95ab087e636342bc7a6dcf
Minh H. Pham Dinh <phdminh01@gmail.com>
2021-12-01 15:19:27 +0700

    added faster vector rotate function, reduced space complexity

--- animate.py
+++ animate.py
@@ -1,58 +1,54 @@
-from scipy.spatial.transform import Rotation as R
-import numpy as np
+from numpy import array, pi, dot
 from shape.generator import draw_donut, draw_box, vector_rotate
 import curses
 
-pi = np.pi
-rows, cols = (40, 80)
-scale = ".,-~:;=!*#$@"
-R1, R2 = (6,6)
-axis_x, axis_y, axis_z = (np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1]))
-init_r = np.array([cols/2,0,cols/2])
-dic, i = draw_donut(R1, R2, init_r)
-# dic, i = draw_box(15,15,30,init_r)
-light = np.array([0,-1,1])
-
-# # animate
-for idx in range(i):
-    r0 = dic[idx]["coord"] - init_r
-    n0 = dic[idx]["n"]
-    r = vector_rotate(r0, pi/6, axis_x) + init_r
-    n = vector_rotate(n0, pi/6, axis_x)
-    dic[idx] = {"coord": r, "n": n}
 
 def draw(terminal):
+    rows, cols = (40, 80)
+    scale = ".,-~:;=!*#$@"
+    axis_x, axis_y, axis_z = (array([1,0,0]), array([0,1,0]), array([0,0,1]))
+    init_r = array([cols/2,0,cols/2])
+    light = array([0,-1,1])
+    R1, R2 = (6,6)
+    dic = draw_donut(R1, R2, init_r)
+    # dic = draw_box(15, 15, 30, init_r)
+    for key in dic.keys():
+        r0 = dic[key]["coord"] - init_r
+        n0 = dic[key]["n"]
+        r = vector_rotate(r0, pi/6, axis_x) + init_r
+        n = vector_rotate(n0, pi/6, axis_x)
+        dic[key] = {"coord": r, "n": n}
     terminal.clear()
     terminal.refresh()
-    theta = 0
+    dtheta = pi/50
     k = 0
     terminal.nodelay(True)
     while k != ord('q'):
-        points = []
-        shade = []
         k = terminal.getch()
-        for idx in range(i):
-            r0 = dic[idx]["coord"]- init_r
-            n0 = dic[idx]["n"]
-            r = vector_rotate(r0, theta, axis_z) + init_r
-            n = vector_rotate(n0, theta, axis_z)
-            points.append(r.tolist())
-            shade.append(np.dot(n, light))
+        for key in dic.keys():
+            r0 = dic[key]["coord"]- init_r
+            n0 = dic[key]["n"]
+            r = vector_rotate(r0, dtheta, axis_z) + init_r
+            n = vector_rotate(n0, dtheta, axis_z)
+            dic[key] = {"coord": r,"n": n, "L": dot(n, light)}
+
+        dic = dict(sorted(dic.items(), key=lambda item: item[1]["coord"][1]))
 
         terminal.clear()
         curses.resize_term(rows, cols)
         terminal.border(0)
-        shade = [x for _, x in sorted(zip(points, shade), key=lambda pair: pair[0][1])]
-        points.sort(key=lambda x: x[1])
-        min_illu, max_illu  = min(shade), max(shade)
-        for idx in range(i):
-            x, y, z = points[idx]
-            illu = (shade[idx] - min_illu)*11/(max_illu - min_illu)
+        values_arr = [item['L'] for item in dic.values()]
+        min_illu, max_illu  = min(values_arr), max(values_arr)
+        for key in dic.keys():
+            x, y, z = dic[key]["coord"]
+            L = dic[key]["L"]
+            illu = (L - min_illu)*11/(max_illu - min_illu)
             string = scale[round(illu)]
             terminal.addstr(round(z * rows/cols), round(x), string, curses.A_BOLD)
-        theta += 0.05
         terminal.move(0,0)
         terminal.refresh()
 
 
-curses.wrapper(draw)
+# draw(0)
+if __name__ == "__main__":
+    curses.wrapper(draw)
--- shape/generator.py
+++ shape/generator.py
@@ -1,17 +1,26 @@
-from scipy.spatial.transform import Rotation as R
-import numpy as np
-pi = np.pi
+from numpy import asarray, array, linspace, dot
+from math import pi, sin, cos, sqrt
 
-axis_x, axis_y, axis_z = (np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1]))
+axis_x, axis_y, axis_z = (array([1,0,0]), array([0,1,0]), array([0,0,1]))
+
+def rotation_matrix(axis, theta):
+    axis = asarray(axis)
+    axis = axis / sqrt(dot(axis, axis))
+    a = cos(theta / 2.0)
+    b, c, d = -axis * sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
 
 def vector_rotate(vec, angle, axis):
-    rotation_vector = axis * angle
-    rotation = R.from_rotvec(rotation_vector)
-    return rotation.apply(vec)
+    M0 = rotation_matrix(axis, angle)
+    return dot(M0, vec)
 
 def draw_donut(R1, R2, init_r):
     dic = {}
-    theta_arr = np.linspace(0, 2*pi, 60).tolist()
+    theta_arr = linspace(0, 2*pi, 60).tolist()
     circle = []
     surface_normal = []
     i = 0
@@ -32,19 +41,20 @@ def draw_donut(R1, R2, init_r):
             dic[i] = {"coord": r, "n": n}
             i += 1
 
-    return dic, i
+    print(i)
+    return dic
 
 
 def draw_box(w, l, h, init_r):
-    width = np.linspace(-w/2, w/2, 15)
-    length = np.linspace(-l/2, l/2, 15)
-    height = np.linspace(-h/2, h/2, 15)
+    width = linspace(-w/2, w/2, 15)
+    length = linspace(-l/2, l/2, 15)
+    height = linspace(-h/2, h/2, 15)
     dic = {}
     i = 0
     for x in width:
         for y in length:
-            r = init_r + np.array([x, y, -h/2])
-            n = np.array([0,0,-1])
+            r = init_r + array([x, y, -h/2])
+            n = array([0,0,-1])
             dic[i] = {"coord": r, "n": n}
             i += 1
 
@@ -52,24 +62,27 @@ def draw_box(w, l, h, init_r):
         x0, y0, z0 = (dic[j]["coord"] - init_r).tolist()
         if x0 in {-w/2, w/2} or y0 in {-l/2, l/2}:
             for z in height:
-                r = init_r + np.array([x0, y0, z])
+                r = init_r + array([x0, y0, z])
                 if  -l/2 <= y0 < l/2:
                     if x0 == -w/2:
-                        n = np.array([-1,0,0])
+                        n = array([-1,0,0])
                     elif x0 == w/2:
-                        n = np.array([1,0,0])
+                        n = array([1,0,0])
                 elif  -w/2 <= x0 < w/2:
                     if y0 == -l/2:
-                        n = np.array([0,-1,0])
+                        n = array([0,-1,0])
                     elif y0 == l/2:
-                        n = np.array([0,1,0])
+                        n = array([0,1,0])
                 dic[i] = {"coord": r, "n": n}
                 i += 1
 
         else:
-            r = init_r + np.array([x0, y0, h/2])
-            n = np.array([0,0,1])
+            r = init_r + array([x0, y0, h/2])
+            n = array([0,0,1])
             dic[i] = {"coord": r, "n": n}
             i += 1
     # print(count)
-    return dic, i
+    return dic
+
+
+# draw_donut(5,5,np.array([0,0,0]))
