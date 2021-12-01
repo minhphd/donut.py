5d6bd1153af52101c6680dfe125940c7d8dd0ab9
Minh H. Pham Dinh <phdminh01@gmail.com>
2021-12-01 11:07:59 +0700

    initial commit

--- animate.py
+++ animate.py
@@ -1,29 +1,24 @@
-from math import pi
 from scipy.spatial.transform import Rotation as R
 import numpy as np
-from shape.generator import draw_donut, draw_box
+from shape.generator import draw_donut, draw_box, vector_rotate
 import curses
 
+pi = np.pi
 rows, cols = (40, 80)
 scale = ".,-~:;=!*#$@"
 R1, R2 = (6,6)
 axis_x, axis_y, axis_z = (np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1]))
 init_r = np.array([cols/2,0,cols/2])
-# dic, i = draw_donut(R1, R2, init_r)
-dic, i = draw_box(15,15,30,init_r)
+dic, i = draw_donut(R1, R2, init_r)
+# dic, i = draw_box(15,15,30,init_r)
 light = np.array([0,-1,1])
-
-def vector_rotate(vec, angle, axis):
-    rotation_vector = axis * angle
-    rotation = R.from_rotvec(rotation_vector)
-    return rotation.apply(vec)
 
 # # animate
 for idx in range(i):
     r0 = dic[idx]["coord"] - init_r
     n0 = dic[idx]["n"]
-    r = vector_rotate(r0, -pi/3, axis_x) + init_r
-    n = vector_rotate(n0, -pi/3, axis_x)
+    r = vector_rotate(r0, pi/6, axis_x) + init_r
+    n = vector_rotate(n0, pi/6, axis_x)
     dic[idx] = {"coord": r, "n": n}
 
 def draw(terminal):
--- shape/generator.py
+++ shape/generator.py
@@ -1,7 +1,6 @@
-from math import pi
 from scipy.spatial.transform import Rotation as R
 import numpy as np
-
+pi = np.pi
 
 axis_x, axis_y, axis_z = (np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1]))
 
@@ -12,7 +11,7 @@ def vector_rotate(vec, angle, axis):
 
 def draw_donut(R1, R2, init_r):
     dic = {}
-    theta_arr = np.arange(0, 2*pi + 0.1, 0.1).tolist()
+    theta_arr = np.linspace(0, 2*pi, 60).tolist()
     circle = []
     surface_normal = []
     i = 0
